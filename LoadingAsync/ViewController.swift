a247dcda0c2935849e00773ce2636a7286bd6231
Tsungyu Yu <ytyubox@gmail.com>
2020-01-08 18:52:24 +0800

    apply combine

--- LoadingAsync/ViewController.swift
+++ LoadingAsync/ViewController.swift
@@ -7,14 +7,44 @@
 //
 
 import UIKit
+import Combine
 
 class ViewController: UIViewController {
-
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+		showLoading()
 	}
-
-
+	
+	private let queue = DispatchQueue(label: "ViewController")
+	var anycancelable: AnyCancellable!
+	let vc:UIAlertController = {
+		let alert = UIAlertController(title: nil, message: "Please wait...", preferredStyle: .alert)
+		let loadingIndicator = UIActivityIndicatorView(frame: CGRect(x: 10, y: 5, width: 50, height: 50))
+		loadingIndicator.hidesWhenStopped = true
+		loadingIndicator.style = .medium
+		loadingIndicator.startAnimating();
+		
+		alert.view.addSubview(loadingIndicator)
+		return alert
+	}()
+	
+	fileprivate func showLoading() {
+		let loadTime = (1...3).randomElement()!
+		anycancelable =
+			Just<Void>(())
+				.delay(for: .seconds(loadTime),
+					   scheduler: queue)
+				.receive(on: RunLoop.main)
+				.sink {
+					self.dismiss(animated: true, completion: nil)
+		}
+		
+		present(vc, animated: true, completion: nil)
+	}
+	
+	@IBAction func didTapButton(_ sender: UIButton) {
+		showLoading()
+	}
+	
 }
-
